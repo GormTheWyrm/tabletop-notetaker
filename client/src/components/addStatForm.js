1739b474ee5132f2b5479d24d19262682711155d
Gorm <gdsoucie@gmail.com>
2020-10-06 00:10:04 -0400

    first input works

---
+++ client/src/components/addStatForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../App.css';
+
+class AddStatForm extends React.Component {
+    constructor(props) {
+      super(props); //why is this complaining?
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+      console.log(event.target.value); //remove this!
+    }
+  
+    handleSubmit(event) {
+    //   alert('A name was submitted: ' + this.state.value);
+    // all I need is to get a simple value to add to state...
+    //use this function to pass value into other state... too tired to do that tonight
+    // alert("submit");
+    console.log(this.state.value);
+    console.log("this is the submit function for the child, this is the wrong function")
+    event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+        {/* <form onClick={this.addHandler}> */}
+          <label>
+            <span>{this.props.resName}: {this.props.resCurrent} / {this.props.resMax}</span>
+            <input className="my-form" type="number" value={this.state.value} onChange={this.handleChange} 
+            name={this.props.name}
+            // look up names and forms...
+            />
+          </label>
+          <input type="submit" value="Add" />
+          {/* do I need a name variable? shuld be name={this.state.resource[index].resourceName + "form"} */}
+        </form>
+      );
+    }
+  }
+  export default AddStatForm;
+  // may wat to extract this into a smaller component that does not display the actual data...
+
+
+  // how to target value? oh, right, state
--- client/src/components/sections/CharacterHeader.js
+++ client/src/components/sections/CharacterHeader.js
@@ -1,33 +1,46 @@
 
 import React from 'react';
 import '../../App.css';
+// import AddStatForm from '../addStatForm';
+// I may need to make the input its own component
+
+// https://stackoverflow.com/questions/43687964/only-numbers-input-number-in-react
 
 /*
 potential bug if resources have the same name property. (form name is resourceName)
 */
 class CharacterHeader extends React.Component {
+  constructor(props) {
+    super(); //super(props); 
 
-  state = {
-    characterName: "Test Character1",
-    resource: [{
-      resourceName: "HP",
-      min: 0,
-      max: 15,
-      current: 15
-    },
-    {
-      resourceName: "Stamina",
-      min: 0,
-      max: 15,
-      current: 7
+    this.state = {
+      characterName: "Test Character1",
+      resource: [{
+        resourceName: "HP",
+        min: 0,
+        max: 15,
+        current: 15,
+        addValue: 0
+      },
+      {
+        resourceName: "Stamina",
+        min: 0,
+        max: 15,
+        current: 7,
+        addValue: 0
+      }
+      ],
+      inputValue1: 0
     }
-  ]
+    this.handleAdd = this.handleAdd.bind(this); // is this needed? This is reason I have constructor
   }
+
   //init function to pull data from database.
   //onChange function
   //on save function to send daa to database...
   //onClick to add new resource
   //addResource to add that resource to the db... ?
+  //need to add a increase max button
 
 
   //replace this with database content
@@ -35,13 +48,74 @@ class CharacterHeader extends React.Component {
   handleNameChange = (event) => {
     this.setState({ characterName: event.target.value });
   } //need a way for this function to determine which section is being changed...
-// lets make this a modal instead!
+  // lets make this a modal instead!
   //onChange is a event for form fields...
+
+
   testFunction = (event) => {
     this.setState({});
     console.log(this.state);
     console.log(event.target.value);
   } //on save right now.... fix!
+
+  handleAdd = (event, resNum) => {
+    event.preventDefault();
+    let current = this.state.resource[resNum].current;
+    let inputValue = this.state.inputValue1;  //this should eventually be this.state.resource[resNum].addValue
+    let newValue = current + inputValue;  //these will need to be numbers- FIXME!
+    let maxValue = this.state.resource[resNum].max;
+    let minValue = this.state.resource[resNum].min;
+    if(newValue > maxValue){
+      newValue = maxValue;
+    } else if (newValue < minValue){
+      newValue = minValue;
+    }
+    let tempObject = this.state.resource;
+    tempObject[resNum].current = newValue;
+    this.setState({resource: tempObject});
+
+console.log(this.state);
+  } //on save right now.... fix!
+
+  // https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+  //increase max button and function...
+
+
+  //ise spread operator to add new resource
+
+
+
+  handleInputChange = (event) => {
+    // console.log(event.target);
+
+
+    // for(let i=0; i++; i<this.state.resource.length){
+    //   if(event.target.name === this.state.resource[i].name){
+    //     let tempRes = this.state.resource;
+    //     tempRes[i].addValue = event.target.value;
+    //     this.setState({resource: tempRes});
+        
+    //   }
+    //   console.log(this.state);
+    // }
+
+    const target = event.target;  //this is a string
+    
+    console.log(event.target.value);
+    const value = parseInt(target.value); //this should be an integer now
+    // const name = target.name;  //I dont know why this was here...
+    
+
+    // console.log(parseInt(testNum);
+    this.setState({
+      // [name]: value
+      inputValue1: value
+    });
+  }
+
+
+
+
   render() {
     return (
 
@@ -50,17 +124,19 @@ class CharacterHeader extends React.Component {
         <h3>{this.state.characterName}</h3>
 
         {this.state.resource.map((resource, index) =>
-
-          <div>
+          // map this via AddForm component
+          <div key={this.state.resource[index].resourceName}>
             {/* this should display all resources in state */}
-            <form className="form-inline">
-            <span>{this.state.resource[index].resourceName} {this.state.resource[index].current} / {this.state.resource[index].max} </span>
-            
-              <label> 
-                 <input type="number" name={this.state.resource[index].resourceName} className='my-form'/>
+          
+
+            <form className="form-inline" onSubmit={(e) => this.handleAdd(e, index)}>
+              <span>{this.state.resource[index].resourceName} {this.state.resource[index].current} / {this.state.resource[index].max} </span>
+
+              <label>
+                <input type="number" name={this.state.resource[index].resourceName} className='my-form' onChange={this.handleInputChange} 
+                value={this.state.inputValue1}/>
               </label>
               <input type="submit" value="Add" />
-              {/* this should set the value of the resource to the state plus the value of the form... */}
             </form>
           </div>
 
@@ -70,6 +146,7 @@ class CharacterHeader extends React.Component {
         <button type="button" className="btn btn-secondary btn-sm">Add Resource</button>
         <button type="button" className="btn btn-secondary btn-sm" onClick={this.testFunction}>Save Section</button>
         {/* save button should be in each location that can be saved, or entire section only has 1? */}
+
 
       </div>
     );
@@ -86,3 +163,26 @@ export default CharacterHeader;
 //only section that is not removable?!
 
 //would be nice to add functionality that adds and subtracts for the player- maybe even showing the final number before saving the value...
+
+
+
+/*
+            <form className="form-inline" onSubmit={(e) => this.handleAdd(e, index)}>
+              <span>{this.state.resource[index].resourceName} {this.state.resource[index].current} / {this.state.resource[index].max} </span>
+
+              <label>
+                <input type="number" name={this.state.resource[index].resourceName + "form"} className='my-form' />
+              </label>
+              <input type="submit" value="Add" />
+              </form>
+
+
+
+
+              broken:
+
+                <AddStatForm resName={this.state.resource[index].resourceName}
+              resCurrent={this.state.resource[index].current} resMax={this.state.resource[index].max}
+              addHandler={this.handleAdd} name={this.state.resource[index].resourceName} 
+            />
+*/
